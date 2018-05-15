23d03815ced81bd7f806b4fef13ca98b27d35dec
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2018-05-15 11:17:15 +0200

    Setup the toolbar with the navigation controller

--- app/src/main/java/nl/jovmit/navigation/MainActivity.kt
+++ app/src/main/java/nl/jovmit/navigation/MainActivity.kt
@@ -3,6 +3,7 @@ package nl.jovmit.navigation
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupNavigation() {
         val navController = findNavController(R.id.mainNavigationFragment)
+        setupActionBarWithNavController(navController)
         navigation.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() =
+            findNavController(R.id.mainNavigationFragment).navigateUp()
 }
